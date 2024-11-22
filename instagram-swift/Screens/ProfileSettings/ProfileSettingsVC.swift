f24af10f59d38b7b2a2b61022bdb14e5eace41b0
Tornike Despotashvili <95018466+Desp0o@users.noreply.github.com>
2024-11-22 17:29:12 +0400

    add profile setting vc

---
+++ instagram-swift/Screens/ProfileSettings/ProfileSettingsVC.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileVC.swift
+//  instagram-swift
+//
+//  Created by Despo on 22.11.24.
+//
+
+import UIKit
+
+class ProfileSettingsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupUI()
+    }
+    
+    private func setupUI() {
+        view.backgroundColor = .red
+    }
+}
