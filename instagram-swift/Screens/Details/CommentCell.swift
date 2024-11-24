240425f7eed9dfaa88817ad44a740077b75ec136
Tornike Despotashvili <95018466+Desp0o@users.noreply.github.com>
2024-11-24 21:44:25 +0400

    scrollview

---
+++ instagram-swift/Screens/Details/CommentCell.swift
@@ -0,0 +1,82 @@
+//
+//  CommentCell.swift
+//  instagram-swift
+//
+//  Created by Despo on 24.11.24.
+//
+
+import UIKit
+
+class CommentCell: UICollectionViewCell {
+    private lazy var avatar: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.clipsToBounds = true
+        
+        return image
+    }()
+    
+    private lazy var commentStack: UIStackView = {
+        let stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .vertical
+        stack.spacing = 2
+        stack.distribution = .fill
+        
+        return stack
+    }()
+    
+    private lazy var userName: UILabel = {
+        let label = UILabel()
+        
+        return label
+    }()
+    
+    private lazy var userComment: UILabel = {
+        let label = UILabel()
+        
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        contentView.addSubview(avatar)
+        contentView.addSubview(commentStack)
+        
+        commentStack.addArrangedSubview(userName)
+        commentStack.addArrangedSubview(userComment)
+        
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            avatar.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 0),
+            avatar.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 15),
+            avatar.widthAnchor.constraint(equalToConstant: 24),
+            avatar.heightAnchor.constraint(equalToConstant: 24),
+            
+            commentStack.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 0),
+            commentStack.leadingAnchor.constraint(equalTo: avatar.trailingAnchor, constant: 10)
+        ])
+    }
+    
+    func configureCell(with comment: Comment) {
+        guard let url = URL(string: comment.profilePicture) else {  return }
+        
+        avatar.imageFrom(url: url)
+        avatar.layer.cornerRadius = 12
+        
+        userName.configureCustomText(text: comment.username, color: .primaryBlack, isBold: true, size: 13)
+        userComment.configureCustomText(text: comment.comment, color: .primaryBlack, isBold: false, size: 13)
+    }
+}
--- instagram-swift/Screens/Details/DetailsVC.swift
+++ instagram-swift/Screens/Details/DetailsVC.swift
@@ -10,6 +10,19 @@ import UIKit
 class DetailsVC: UIViewController {
     var customView: CustomPostView
     var model: PostModel
+    
+    private lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
+    
+    private lazy var containerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
 
     private lazy var postView: UIView = {
         let view = UIView()
@@ -52,6 +65,25 @@ class DetailsVC: UIViewController {
         
         return button
     }()
+    
+    private lazy var collection: UICollectionView = {
+        let collection: UICollectionView
+        let collectionLayout = UICollectionViewFlowLayout()
+        collectionLayout.scrollDirection = .horizontal
+        collectionLayout.itemSize = CGSize(width: 100, height: 50)
+        collectionLayout.minimumLineSpacing = 0
+        collectionLayout.collectionView?.bounces = false
+        collection = UICollectionView(frame: .zero, collectionViewLayout: collectionLayout)
+        collection.bounces = false
+        collection.translatesAutoresizingMaskIntoConstraints = false
+        collection.register(CommentCell.self, forCellWithReuseIdentifier: "CommentCell")
+        collection.isPagingEnabled = true
+        collection.delegate = self
+        collection.dataSource = self
+        collection.showsHorizontalScrollIndicator = false
+        collection.backgroundColor = .systemGreen
+        return collection
+    }()
 
     init(customView: CustomPostView = CustomPostView(frame: .zero), model: PostModel) {
         self.customView = customView
@@ -71,20 +103,26 @@ class DetailsVC: UIViewController {
     
     private func setupUI() {
         navigationController?.isNavigationBarHidden = true
-
-        view.addSubview(topBar)
-        view.addSubview(postView)
-        view.addSubview(customView)
+        view.addSubview(scrollView)
         
+        view.addSubview(topBar)
         topBar.addSubview(bottomLine)
         topBar.addSubview(barTitle)
         topBar.addSubview(backButton)
-        view.bringSubviewToFront(topBar)
+        
+        
+        
+        
         customView.setupView(with: model)
-
+        
         postView = customView
         postView.translatesAutoresizingMaskIntoConstraints = false
-
+        
+        scrollView.addSubview(containerView)
+        containerView.addSubview(postView)
+        containerView.addSubview(customView)
+        containerView.addSubview(collection)
+        
         setupConstraints()
         setupButtonAction()
     }
@@ -96,30 +134,62 @@ class DetailsVC: UIViewController {
             topBar.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             topBar.heightAnchor.constraint(equalToConstant: 132),
 
-            postView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            postView.topAnchor.constraint(equalTo: topBar.bottomAnchor),
-            postView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            postView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            
             bottomLine.leadingAnchor.constraint(equalTo: topBar.leadingAnchor),
             bottomLine.trailingAnchor.constraint(equalTo: topBar.trailingAnchor),
             bottomLine.bottomAnchor.constraint(equalTo: topBar.bottomAnchor),
             bottomLine.heightAnchor.constraint(equalToConstant: 1),
-            
+
             barTitle.centerXAnchor.constraint(equalTo: topBar.centerXAnchor),
             barTitle.bottomAnchor.constraint(equalTo: topBar.bottomAnchor, constant: -12),
-            
+
             backButton.bottomAnchor.constraint(equalTo: topBar.bottomAnchor, constant: -12),
             backButton.leadingAnchor.constraint(equalTo: topBar.leadingAnchor, constant: 15),
             backButton.widthAnchor.constraint(equalToConstant: 24),
-            backButton.heightAnchor.constraint(equalToConstant: 24)
+            backButton.heightAnchor.constraint(equalToConstant: 24),
+
+            scrollView.topAnchor.constraint(equalTo: topBar.bottomAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+
+            containerView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            containerView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            containerView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            containerView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            containerView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+
+            postView.topAnchor.constraint(equalTo: containerView.topAnchor),
+            postView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+            postView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
+            postView.heightAnchor.constraint(equalToConstant: 610),
+
+            collection.topAnchor.constraint(equalTo: postView.bottomAnchor, constant: 0),
+            collection.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+            collection.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
+            collection.heightAnchor.constraint(equalToConstant: 700),
+
+            collection.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -16)
         ])
     }
-    
+
     private func setupButtonAction() {
         backButton.addAction(UIAction(handler: {[weak self] _ in
             self?.navigationController?.popViewController(animated: true)
         }), for: .touchUpInside)
     }
     
+}
+
+
+extension DetailsVC:  UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        model.comments.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CommentCell", for: indexPath) as? CommentCell
+        let currentComment = model.comments[indexPath.row]
+        cell?.configureCell(with: currentComment)
+        return cell ?? CommentCell()
+    }
 }
