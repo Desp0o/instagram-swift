e6bb98ff1178d7bba67bfbf92ccee2ad0b1d1a89
KochieviAna <anna.kochievi@gmail.com>
2024-11-24 12:14:36 +0400

    SearchVC "skeleton" added to project.

--- instagram-swift/Screens/Search/SearchVC.swift
+++ instagram-swift/Screens/Search/SearchVC.swift
@@ -7,15 +7,97 @@
 
 import UIKit
 
-class SearchVC: UIViewController {
-
+final class SearchVC: UIViewController {
+    
+    private lazy var searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        searchBar.placeholder = "Search"
+        return searchBar
+    }()
+    
+    private lazy var searchCollectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.register(SearchCollectionViewCell.self, forCellWithReuseIdentifier: SearchCollectionViewCell.identifier)
+        collectionView.backgroundColor = .clear
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        return collectionView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         setupUI()
     }
     
     private func setupUI() {
-        view.backgroundColor = .red
+        view.backgroundColor = .systemBackground
+        
+        view.addSubview(searchBar)
+        view.addSubview(searchCollectionView)
+        
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            searchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
+            searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8),
+            searchBar.heightAnchor.constraint(equalToConstant: 36)
+        ])
+        
+        NSLayoutConstraint.activate([
+            searchCollectionView.topAnchor.constraint(equalTo: searchBar.bottomAnchor, constant: 19),
+            searchCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            searchCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            searchCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+    private func createLayout() -> UICollectionViewLayout {
+        return UICollectionViewCompositionalLayout { sectionIndex, environment in
+            let groupSize = NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .absolute(300)
+            )
+            
+            let group = NSCollectionLayoutGroup.custom(layoutSize: groupSize) { environment in
+                var items: [NSCollectionLayoutGroupCustomItem] = []
+                
+                let largeItemFrame = CGRect(x: 0, y: 0, width: environment.container.contentSize.width * 0.66, height: 300)
+                let largeItem = NSCollectionLayoutGroupCustomItem(frame: largeItemFrame)
+                items.append(largeItem)
+                
+                let smallTopItemFrame = CGRect(x: environment.container.contentSize.width * 0.66 + 4, y: 0, width: environment.container.contentSize.width * 0.34 - 4, height: 148)
+                let smallTopItem = NSCollectionLayoutGroupCustomItem(frame: smallTopItemFrame)
+                items.append(smallTopItem)
+                
+                let smallBottomItemFrame = CGRect(x: environment.container.contentSize.width * 0.66 + 4, y: 152, width: environment.container.contentSize.width * 0.34 - 4, height: 148)
+                let smallBottomItem = NSCollectionLayoutGroupCustomItem(frame: smallBottomItemFrame)
+                items.append(smallBottomItem)
+                
+                return items
+            }
+            
+            let section = NSCollectionLayoutSection(group: group)
+            section.contentInsets = NSDirectionalEdgeInsets(top: 4, leading: 4, bottom: 4, trailing: 4)
+            
+            return section
+        }
     }
 }
+
+extension SearchVC: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SearchCollectionViewCell.identifier, for: indexPath) as? SearchCollectionViewCell else { return UICollectionViewCell() }
+        return cell
+    }
+}
+
